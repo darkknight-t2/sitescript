9aba7c35484b4cca656c26628b9d4f15fcb7f297
darkknight <darkknight.t2@gmail.com>
2010-09-04 23:50:13 +0900

    Update Innolife.site.js 0.2

--- Innolife.site.js
+++ Innolife.site.js
@@ -5,8 +5,8 @@
 // @authorUrl   http://darkknightlabs.com/
 // @scriptUrl   http://darkknightlabs.com/site-script/
 // @description 
-// @date        2009/02/16
-// @version     0.1
+// @date        2010/09/04
+// @version     0.2
 // ==/SiteScript==
 
 
@@ -72,6 +72,15 @@ CravingSiteScript.prototype = {
         return eval( "("+text+")" );
     },
     
+    getResponseBody: function() {
+        if ( this._xhr ) {
+            return this._xhr.responseBody;
+        }
+        else {
+            return null;
+        }
+    },
+    
     /// Math
     random: function( limit ) {
         return Math.floor( Math.random() * limit );
@@ -86,12 +95,88 @@ CravingSiteScript.prototype = {
                   .replace( /&(gt|#62);/ig,      ">"  )
                   .replace( /&(nbsp|#160);/ig,   " "  )
                   .replace( /&(frasl|#8260);/ig, "/"  );
+    },
+    
+    getLocation: function( url ) {
+        var location = null;
+        try {
+            var winHttp = new ActiveXObject( "WinHttp.WinHttpRequest.5.1" );
+            var WinHttpRequestOption_EnableRedirects = 6;
+            
+            winHttp.option( WinHttpRequestOption_EnableRedirects ) = false;
+            winHttp.open( "GET", url, false );
+            winHttp.send();
+            
+            location = winHttp.getResponseHeader( "Location" );
+        }
+        catch( e ) {
+            return null;
+        }
+        return location;
+    }
+}
+
+function CharConv() {
+    var _initialized = false;
+    var _stream = null;
+    var _result = "";
+    
+    this._initialize = function () {
+        try {
+            this._stream = new ActiveXObject( "ADODB.Stream" );
+            this._initialized = true;
+        }
+        catch ( e ) {}
+    }
+    this._initialize();
+    
+    this.isInit = function() {
+        return this._initialized;
+    }
+    
+    this.convert = function ( bin, charset ) {
+        this._stream.Open();
+        try{
+            this._stream.Type = 1;
+            this._stream.Write( bin );
+            
+            this._stream.Position = 0;
+            this._stream.Type = 2;
+            this._stream.Charset = charset; 
+            this._result = this._stream.ReadText();
+        }
+        catch( e ) {
+            this._result = "";
+        }
+        this._stream.Close();
+    }
+    
+    this.getResult = function() {
+        return this._result;
     }
 }
 
 
 function isSiteUrl( url ) {
-    if ( url.match( /http:\/\/www\.innolife\.tv\/list\.php\?ac_id=\d+&ai_id=\d+/ ) ) {
+    if (   url.match( /http:\/\/www\.innolife\.tv\/list1\.php/ )
+        && url.match( /(\?|&)ac_id=\d+/ )
+        && url.match( /(\?|&)ai_id=\d+/ )
+    ) {
+        return true;
+    }
+    
+    if (   url.match( /http:\/\/www\.innolife\.tv\/tv_qacont\.php/ )
+        && url.match( /(\?|&)tbl=tv_board1/ )
+        && url.match( /(\?|&)aq_id=\d+/ )
+        && url.match( /(\?|&)aq_listnum=\d+/)
+    ) {
+        return true;
+    }
+    
+    if (   url.match( /http:\/\/contents\.innolife\.net\/listpre\.php/ )
+        && url.match( /(\?|&)ac_id=\d+/ )
+        && url.match( /(\?|&)ai_id=\d+/ )
+    ) {
         return true;
     }
 }
@@ -105,11 +190,79 @@ function getVideoDetail( url ) {
         return null;
     }
     
-    text.match( /<embed src="(.*?)">/ );
-    var realUrl = RegExp.$1;
+    var charConv = new CharConv();
+    if ( charConv.isInit() ) {
+        charConv.convert( craving.getResponseBody(), "euc-jp" );
+        var result = charConv.getResult();
+        if ( result != "" ) {
+            text = result;
+        }
+    }
+    var title;
+    var realUrl;
     
-    realUrl.match( /.*\/(.*)\.wmv/ );
-    var title = RegExp.$1;
+    if (   url.match( /http:\/\/www\.innolife\.tv\/list1\.php/ )
+        || url.match( /http:\/\/contents\.innolife\.net\/listpre\.php/ )
+    ) {
+        if ( !text.match( /<embed [^>]*?src\s*?=\s*?"([^"]+)"/ ) ) {
+            return null;
+        }
+        realUrl = RegExp.$1;
+        
+        if ( text.match( /<meta name="description" content="([^"]+)"/ ) && charConv.isInit() ) {
+            title = RegExp.$1;
+        }
+        else {
+            url.match( /(\?|&)ac_id=(\d+)/ );
+            var ac_id = RegExp.$2;
+            url.match( /(\?|&)ai_id=(\d+)/ );
+            var ai_id = RegExp.$2;
+            title = "innolife_" + ac_id + "_" + ai_id;
+        }
+    }
+    else if ( url.match( /http:\/\/www\.innolife\.tv\/tv_qacont\.php/ ) ) {
+        if ( !text.match( /<param name='movie' value='([^']+)'/ ) ) {
+            return null;
+        }
+        
+        var loaderUrl = RegExp.$1;
+        if ( text.match( /<tr class=st_bgc_list>\s*?<td align=center>.*?<\/td>\s*?<td>(.*?)<\/td>/m ) && charConv.isInit() ){
+            title = RegExp.$1;
+        }
+        else {
+            url.match( /(\?|&)aq_listnum=(\d+)/ );
+            title = "inno_ucc_" + RegExp.$2;
+        }
+        
+        var location = craving.getLocation( loaderUrl );
+        if ( !location ) {
+            return null;
+        }
+        
+        var param = location.substring( location.indexOf( "?" ) );
+        var xmlUrl = "http://v.nate.com/xmovie_url.php" + param;
+        
+        text = craving.getResponseText( xmlurl );
+        if ( text == null ) {
+            return null;
+        }
+        if (   text.match( /<mov_url>(.*?)<\/mov_url>/ )
+            || text.match( /<mov_url_alt>(.*?)<\/mov_url_alt>/ )
+        ) {
+           realUrl = decodeURIComponent( RegExp.$1 );
+        }
+        else {
+            return null;
+        }
+    }
+    else {
+        return null;
+    }
+    
+    title = title.replace( /&#\d+;/g,
+                           function( chars ) { return String.fromCharCode( parseInt( chars.substr( 2 ) ) ); }
+                         );
+    title = title.replace( /[\\\/:*?"<>|]/g, "_" );
     
     return { videoTitle0: title, videoUrl0: realUrl };
 }
