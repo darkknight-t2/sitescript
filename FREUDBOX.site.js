1c004f4891b8d2537e75f8b33a4b04a5fccfdc0a
darkknight <darkknight.t2@gmail.com>
2010-09-05 18:01:36 +0900

    Update FREUDBOX.site.js 0.2

--- FREUDBOX.site.js
+++ FREUDBOX.site.js
@@ -5,8 +5,8 @@
 // @authorUrl   http://darkknightlabs.com/
 // @scriptUrl   http://darkknightlabs.com/site-script/
 // @description 
-// @date        2009/02/01
-// @version     0.1
+// @date        2010/09/05
+// @version     0.2
 // ==/SiteScript==
 
 
@@ -90,14 +90,99 @@ CravingSiteScript.prototype = {
 }
 
 
+function FleudDecrypt() {
+    this._arrSBox  = new Array();
+    this._arrMyKey = new Array();
+    
+    this.decrypt = function ( strEnc, strKey ){
+        return this._charsToStr( this._calculate( this._hexToChars( strEnc ), this._strToChars( strKey ) ) );
+    }
+    
+    this._charsToStr = function ( arr ){
+        var str = "";
+        for (var i = 0; i < arr.length; i++){
+            str += String.fromCharCode( arr[i] );
+        }
+        return str;
+    }
+    
+    this._calculate = function ( arrEnc, arrKey ){
+        this._initialize( arrKey );
+        
+        var arrRet = new Array();
+        var sd1 = 0;
+        var sd2 = 0;
+        
+        for ( var i = 0; i < arrEnc.length; i++ ) {
+            sd1 = ( sd1 + 1 ) % 256;
+            sd2 = ( sd2 + this._arrSBox[ sd1 ] ) % 256;
+            
+            var temp1 = this._arrSBox[ sd1 ];
+            this._arrSBox[ sd1 ] = this._arrSBox[ sd2 ];
+            this._arrSBox[ sd2 ] = temp1;
+            var temp2 = ( this._arrSBox[ sd1 ] + this._arrSBox[ sd2 ] ) % 256;
+            var ans = arrEnc[ i ] ^ this._arrSBox[ temp2 ];
+            arrRet.push( ans );
+        }
+        return arrRet;
+    }
+    
+    this._initialize = function ( arr ) {
+        for ( var i = 0; i <= 255; i++ ) {
+            this._arrMyKey[ i ] = arr[ i % arr.length ];
+            this._arrSBox[ i ] = i;
+        }
+        
+        var temp1 = 0; 
+        for ( var i = 0; i <= 255; i++ ) {
+            temp1 = ( temp1 + this._arrSBox[ i ] + this._arrMyKey[ i ] ) % 256;
+            var temp2 = this._arrSBox[ i ];
+            this._arrSBox[ i ] = this._arrSBox[ temp1 ];
+            this._arrSBox[ temp1 ] = temp2;
+        }
+        return;
+    }
+    
+    this._hexToChars = function ( str ){
+        var arr = new Array();
+        var headx;
+        if ( str.substr( 0,2 ) == "0x" ) {
+            headx = 2;
+        }
+        else {
+            headx = 0;
+        }
+            
+        for ( var i = headx; i < str.length; i+= 2 ) {
+            arr.push( parseInt( str.substr( i, 2 ), 16 ) );
+        }
+        return arr;
+    }
+    
+    this._strToChars = function ( str ){
+        var arr = new Array();
+        for ( var i = 0; i < str.length; i++ ) {
+            arr.push( str.charCodeAt( i ) );
+        }
+        return arr;
+    }
+}
+
+
 function isSiteUrl( url ) {
-    if ( url.match( /http:\/\/www\.freudbox\.com\/video\/.*/ ) ) {
+    if ( url.match( /http:\/\/(www\.)?freudbox\.com\/video_\d+/ ) ) {
         return true;
     }
 }
 
 
 function getVideoDetail( url ) {
+    url = url.replace( /http:\/\/freudbox\.com\//, "http://www.freudbox.com/" );
+    if ( !url.match( /http:\/\/www\.freudbox\.com\/video_(\d+)/ ) ) {
+        return null;
+    }
+    var videoId = RegExp.$1;
+    
     var craving = new CravingSiteScript();
     var text = craving.getResponseText( url );
     
@@ -105,10 +190,18 @@ function getVideoDetail( url ) {
         return null;
     }
     
-    text.match( /<h2 title=".*">(.*?)<\/h2>/ );
-    var title = RegExp.$1;
+    var title;
+    if ( text.match( /<h2>(.*?)<\/h2>/ ) ) {
+        title = RegExp.$1;
+    }
+    else {
+        title = "freudbox_" + videoId;
+    }
+    title = title.replace( /[\\\/:*?"<>|]/g, "_" );
     
-    text.match( /so\.addVariable\("config",\s*"(.*?)"\)/ );
+    if ( !text.match( /<param name="flashvars" value="file=(.*?)" \/>/ ) ) {
+        return null;
+    }
     var xmlUrl = RegExp.$1;
     
     text = craving.getResponseText( xmlUrl );
@@ -117,7 +210,11 @@ function getVideoDetail( url ) {
         return null;
     }
     
-    text.match( /<PLAYER_SETTINGS Name="FLVPath" Value="(.*?)"/ );
+    text = ( new FleudDecrypt() ).decrypt( text, "tW5Fs1,ra" );
+    
+    if ( !text.match( /<location>(.*?)<\/location>/ ) ) {
+        return null;
+    }
     var realUrl = RegExp.$1;
     
     return { videoTitle0: title, videoUrl0: realUrl };
