457db8d1b12c61f76c5fed0d0c516ffe25e997c6
darkknight <darkknight.t2@gmail.com>
2010-09-19 10:03:29 +0900

    Update RedTube.site.js 0.3

--- RedTube.site.js
+++ RedTube.site.js
@@ -5,8 +5,8 @@
 // @authorUrl   http://darkknightlabs.com/
 // @scriptUrl   http://darkknightlabs.com/site-script/
 // @description 
-// @date        2008/10/17
-// @version     0.2
+// @date        2010/09/19
+// @version     0.3
 // ==/SiteScript==
 
 
@@ -105,76 +105,13 @@ function getVideoDetail( url ) {
         return null;
     }
     
-    text.match( /<h1 class='videoTitle'>(.*?)</ );
+    text.match( /<h1 class="videoTitle">(.*?)</ );
     var title = RegExp.$1;
     
-    text.match( /so\.addVariable\("id", "(\d+)"\)/ );
-    var id = RegExp.$1;
-    
-    text.match( /so\.addVariable\("styleURL", "(.*?)"\)/ );
-    var styleUrl = "http://www.redtube.com" + RegExp.$1;
-    
-    text = craving.getResponseText( styleUrl );
-    
-    if ( text == null ) {
+    if ( !text.match( /hashlink=([^&"']+)/ ) ) {
         return null;
     }
-    
-    text.match( /config\.stream{\r\n\tURL: (.*?);/ );
-    var streamUrl = RegExp.$1;
-    
-    var s = "" + id;
-    if ( s == "") {
-        s = "1";
-    }
-    
-    var pathnr = "" + Math.floor( parseFloat( s ) / 1000 );
-    var l = s.length;
-    
-    for ( var i = 1; i <= 7 - l; i++ ) {
-        s = "0" + s;
-    }
-    
-    l = pathnr.length;
-    
-    for ( var i = 1; i <= 7 - l; i++ ) {
-        pathnr = "0" + pathnr;
-    }
-    
-    var xc = new Array( "R", "1", "5", "3", "4", "2", "O", "7", "K", "9", "H", "B", "C", "D", "X", "F", "G", "A", "I", "J", "8", "L", "M", "Z", "6", "P", "Q", "0", "S", "T", "U", "V", "W", "E", "Y", "N" );
-    var code = "";
-    var qsum = 0;
-    
-    for ( var i = 0; i <= 6; i++ ) {
-        qsum = qsum + parseInt( s.charAt( i ) ) * ( i + 1 );
-    }
-    
-    var s1 = "" + qsum;
-    qsum = 0;
-    
-    for ( var i = 0; i < s1.length; i++ ) {
-        qsum = qsum + parseInt( s1.charAt( i ) );
-    }
-    
-    var qstr;
-    if ( qsum >= 10 ) {
-        qstr = "" + qsum;
-    }
-    else {
-        qstr = "0" + qsum;
-    }
-    
-    code = code + xc[ s.charCodeAt( 3 ) - 48 + qsum + 3 ];
-    code = code + qstr.charAt( 1 );
-    code = code + xc[ s.charCodeAt( 0 ) - 48 + qsum + 2 ];
-    code = code + xc[ s.charCodeAt( 2 ) - 48 + qsum + 1 ];
-    code = code + xc[ s.charCodeAt( 5 ) - 48 + qsum + 6 ];
-    code = code + xc[ s.charCodeAt( 1 ) - 48 + qsum + 5 ];
-    code = code + qstr.charAt( 0 );
-    code = code + xc[ s.charCodeAt( 4 ) - 48 + qsum + 7 ];
-    code = code + xc[ s.charCodeAt( 6 ) - 48 + qsum + 4 ];
-    
-    var realUrl = streamUrl + pathnr + "/" + code + ".flv";;
+    var realUrl = decodeURIComponent( RegExp.$1 );
     
     return { videoTitle0: title, videoUrl0: realUrl };
 }
